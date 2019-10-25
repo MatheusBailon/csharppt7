23fdc775f1b809b070592c86ad40f28b8ad94a5d
Matheus Henrique <matheus_henrique06@hotmail.com>
2019-10-25 13:02:27 -0300

    Classe adicionada para testar commit no github

---
+++ ByteBank.Modelos/ContaPoupanca.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    public class ContaPoupanca : ContaCorrente
+    {
+        public ContaPoupanca(int agencia,int numero):base(agencia,numero)
+        {
+        }
+        public int Agencia {get;}
+        public int Numero { get; }
+
+
+    }
+}
